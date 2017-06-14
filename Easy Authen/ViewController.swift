60b39047925f3134dadacbca1d879e5886dfba2f
Student18 <axiom2517@gmail.com>
2017-06-14 11:33:33 +0700

    authen success

--- Easy Authen/ViewController.swift
+++ Easy Authen/ViewController.swift
@@ -2,32 +2,109 @@
 //  ViewController.swift
 //  Easy Authen
 //
-//  Created by Student18 on 6/13/2560 BE.
+//  Created by Student12 on 6/13/2560 BE.
 //  Copyright © 2560 SNRU. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var passWordTextField: UITextField!
+    // Implicit
+    var strUser: String?
+    var strPassword: String?
+    
+    
+    
+    
+    
     @IBOutlet weak var userTextField: UITextField!
-
-    @IBOutlet weak var messageLabel: UIButton!
-
-    @IBAction func loginMethod(_ sender: Any) {
-    }
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
+    
+    @IBAction func loginButtom(_ sender: Any) {
+        
+        messageLabel.text = " "
+        
+        
+        // Get Value From TextField
+        strUser = userTextField.text
+        strPassword = passwordTextField.text
+        
+        // show User and Pass on Console
+        print("User ==> \(strUser!)")
+        print("Password ==> \(strPassword!)")
+        
+        // Find Count String
+        let intUser  = strUser?.characters.count
+        let intPass = strPassword?.characters.count
+        
+        
+        
+        print("intUser ==? \(intUser!)")
+        print("intPass ==> \(intPass!)")
+        
+        // Call checkspace
+        if checkSpace(myString: strUser!) && checkSpace(myString: strPassword!) {
+            print("No Space")
+            showMessage(strMessage: "")
+        }  else {
+            print("Have Space")
+            showMessage(strMessage: "Please Fill Every Blank")
+        }
+        
+        
+        
+        
+        
+        
+    }  // loginButton
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }  // Main Method
+    
+    func showMessage(strMessage: String) -> Void {
+        messageLabel.text = strMessage
+        
     }
-
+    
+    func checkSpace(myString: String) -> Bool {
+        let intString = myString.characters.count
+        var result: Bool = true
+        
+        if intString == 0 {
+            // No Space
+            result = false
+        }
+        
+        
+        return result
+        
+    }
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-  
+    }  // didRecive
+    
 }
-
